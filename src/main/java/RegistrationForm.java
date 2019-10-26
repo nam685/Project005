273f289ef7e284bf8d9daaa5699e0fd4d7931ed2
Nam Le <LeHaiNam2371999@gmail.com>
2019-10-26 12:11:20 +0200

    Commit 4. Added mock GUI form.

--- src/main/java/RegistrationForm.java
+++ src/main/java/RegistrationForm.java
@@ -1,4 +1,31 @@
 package main.java;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RegistrationForm {
+    private JTextField input;
+    private JButton clickMeToSubmitButton;
+    private JTextArea output;
+    private JPanel myPanel;
+    private JTextPane registrationFormTextPane;
+
+    public RegistrationForm() {
+        clickMeToSubmitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                output.setText(input.getText());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("RegistrationForm");
+        frame.setContentPane(new RegistrationForm().myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
